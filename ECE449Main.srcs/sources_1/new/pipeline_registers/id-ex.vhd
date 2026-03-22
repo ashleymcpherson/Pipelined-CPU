6392647f79f32716e83172de5b4006baeb3209a7
ashleymcpherson <156880729+ashleymcpherson@users.noreply.github.com>
2026-03-22 15:31:15 -0700

    Update id-ex.vhd

--- ECE449Main.srcs/sources_1/new/pipeline_registers/id-ex.vhd
+++ ECE449Main.srcs/sources_1/new/pipeline_registers/id-ex.vhd
@@ -3,62 +3,71 @@ use ieee.std_logic_1164.all;
 
 entity id_ex_register is
     port(
-        clock : in std_logic;
-        reset : in std_logic;
+        clock         : in std_logic;
+        reset         : in std_logic;
+        flush         : in std_logic;
 
-        opcode_in : in std_logic_vector(6 downto 0);
-        ra_dest_in : in std_logic_vector(2 downto 0);
-        rb_val_in : in std_logic_vector(15 downto 0);
-        rc_val_in : in std_logic_vector(15 downto 0);
-        reg_write_in : in std_logic;  
-        shiftop_in : in std_logic_vector(3 downto 0); 
-                         -- control signal
+        instr_in      : in std_logic_vector(15 downto 0);
+        pc_in         : in std_logic_vector(15 downto 0);
+        ra_dest_in    : in std_logic_vector(2 downto 0);
+        rb_src_in     : in std_logic_vector(2 downto 0);
+        rc_src_in     : in std_logic_vector(2 downto 0);
+        rb_val_in     : in std_logic_vector(15 downto 0);
+        rc_val_in     : in std_logic_vector(15 downto 0);
+        reg_write_in  : in std_logic;
 
-        opcode_out : out std_logic_vector(6 downto 0);
-        ra_dest_out : out std_logic_vector(2 downto 0);
-        rb_val_out : out std_logic_vector(15 downto 0);
-        rc_val_out : out std_logic_vector(15 downto 0);
-        reg_write_out : out std_logic;
-        shiftop_out : out std_logic_vector(3 downto 0)
+        instr_out     : out std_logic_vector(15 downto 0);
+        pc_out        : out std_logic_vector(15 downto 0);
+        ra_dest_out   : out std_logic_vector(2 downto 0);
+        rb_src_out    : out std_logic_vector(2 downto 0);
+        rc_src_out    : out std_logic_vector(2 downto 0);
+        rb_val_out    : out std_logic_vector(15 downto 0);
+        rc_val_out    : out std_logic_vector(15 downto 0);
+        reg_write_out : out std_logic
     );
 end entity;
 
 architecture rtl of id_ex_register is
-    signal opcode_reg : std_logic_vector(6 downto 0);
-    signal ra_reg : std_logic_vector(2 downto 0);
-    signal rb_reg : std_logic_vector(15 downto 0);
-    signal rc_reg : std_logic_vector(15 downto 0);
-    signal write_enable_reg: std_logic;
-    signal shift_op_reg: std_logic_vector(3 downto 0);
+    signal instr_reg        : std_logic_vector(15 downto 0);
+    signal pc_reg           : std_logic_vector(15 downto 0);
+    signal ra_reg           : std_logic_vector(2 downto 0);
+    signal rb_src_reg       : std_logic_vector(2 downto 0);
+    signal rc_src_reg       : std_logic_vector(2 downto 0);
+    signal rb_reg           : std_logic_vector(15 downto 0);
+    signal rc_reg           : std_logic_vector(15 downto 0);
+    signal write_enable_reg : std_logic;
 begin
-
     process(clock)
     begin
         if rising_edge(clock) then
-            if reset = '1' then
-                shift_op_reg <= (others => '0');
-                opcode_reg <= (others => '0');
-                ra_reg <= (others => '0');
-                rb_reg <= (others => '0');
-                rc_reg <= (others => '0');
+            if reset = '1' or flush = '1' then
+                instr_reg        <= (others => '0');
+                pc_reg           <= (others => '0');
+                ra_reg           <= (others => '0');
+                rb_src_reg       <= (others => '0');
+                rc_src_reg       <= (others => '0');
+                rb_reg           <= (others => '0');
+                rc_reg           <= (others => '0');
                 write_enable_reg <= '0';
             else
-                shift_op_reg <= shiftop_in;
-                opcode_reg <= opcode_in;
-                ra_reg <= ra_dest_in;
-                rb_reg <= rb_val_in;
-                rc_reg <= rc_val_in;
+                instr_reg        <= instr_in;
+                pc_reg           <= pc_in;
+                ra_reg           <= ra_dest_in;
+                rb_src_reg       <= rb_src_in;
+                rc_src_reg       <= rc_src_in;
+                rb_reg           <= rb_val_in;
+                rc_reg           <= rc_val_in;
                 write_enable_reg <= reg_write_in;
             end if;
         end if;
     end process;
-    
-    shiftop_out <= shift_op_reg;
-    opcode_out <= opcode_reg;
-    ra_dest_out <= ra_reg;
-    rb_val_out <= rb_reg;
-    rc_val_out <= rc_reg;
-    reg_write_out <= write_enable_reg;
 
+    instr_out     <= instr_reg;
+    pc_out        <= pc_reg;
+    ra_dest_out   <= ra_reg;
+    rb_src_out    <= rb_src_reg;
+    rc_src_out    <= rc_src_reg;
+    rb_val_out    <= rb_reg;
+    rc_val_out    <= rc_reg;
+    reg_write_out <= write_enable_reg;
 end architecture;
-                
