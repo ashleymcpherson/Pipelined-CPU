61862af9205dc9589cbe9bd505aca2850cdd4bf3
ashleymcpherson <156880729+ashleymcpherson@users.noreply.github.com>
2026-03-22 13:58:22 -0700

    Update ALUv2.vhd

--- ECE449Main.srcs/sources_1/new/ALUv2.vhd
+++ ECE449Main.srcs/sources_1/new/ALUv2.vhd
@@ -39,62 +39,46 @@ Port (
     opcode : in std_logic_vector(6 downto 0);
     shiftop : in std_logic_vector(3 downto 0);
     outside_input : in std_logic_vector(15 downto 0);
-    --Cin : in std_logic_vector(1);
-    output : out std_logic_vector(15 downto 0)
-    --Cout : out std_logic;
-    --flag : out std_logic_vector(3 downto 0)
+    output : out std_logic_vector(15 downto 0);
+    z_flag : out std_logic;
+    n_flag : out std_logic
 );
 end ALUv2;
 
 architecture Behavioral of ALUv2 is
-    --signal temp : std_logic_vector(15 downto 0);
-    signal result_int : std_logic_vector(15 downto 0);
-    signal carry : std_logic;
-    signal overflow : std_logic;
-    
-    --signal outside_input : std_logic_vector(15 downto 0);
     signal outside_output : std_logic_vector(15 downto 0);
-    
-    --signal overflow : std_logic;
-    --signal negative : std_logic;
-    --signal carry : std_logic;
     
 begin
     process(opcode, rb, rc, outside_input, shiftop)
-        variable temp_u : signed(16 downto 0);
         variable temp_s : signed(15 downto 0);
         variable mul_temp : signed(31 downto 0);
         
     
     begin
-        result_int <= (others => '0');
-        carry <= '0';
-        overflow <= '0';
+        z_flag <= '0';
+        n_flag <= '0';
         
         
         case opcode is
-            when "0000001" => 
-               
+            when "0000001" => -- add
                 temp_s := signed(rb) + signed(rc);
-          
                 output <= std_logic_vector(temp_s);
                 
-            when "0000010" => 
+            when "0000010" => -- sub
                 temp_s := signed(rb) - signed(rc); --EROROROR HERE
                 output <= std_logic_vector(temp_s);
                 
-            
-            when "0000011" => 
+            when "0000011" => -- mul
                 mul_temp := rb * rc;
                 output <= std_logic_vector(mul_temp(15 downto 0));
             
-            when "0000100" =>
+            when "0000100" => -- nand
                 output <= std_logic_vector(rb) nand std_logic_vector(rc);
             
-            when "0000101"=>
+            when "0000101"=> -- shl
                 output <= std_logic_vector(shift_left(unsigned(rb), to_integer(unsigned(shiftop))));
                 
-            when "0000110"=>
+            when "0000110"=> -- shr
                 output <= std_logic_vector(shift_right(unsigned(rb), to_integer(unsigned(shiftop))));
                 
             when "0100000" => -- output
@@ -104,10 +88,21 @@ begin
             when "0100001" => -- input
                 output <= outside_input;
                 
+            when "0000111" => -- test
+                output <= std_logic_vector(rb);
+                
+                if rb = to_signed(0, 16) then
+                    z_flag <= '1';
+                end if;
+                
+                if rb < to_signed(0, 16) then
+                    n_flag <= '1';
+                end if;
+                
             when others => 
                 output <= (others => '0');
         end case;
-            --output <= result_int;
+
     end process;
 
 end Behavioral;
