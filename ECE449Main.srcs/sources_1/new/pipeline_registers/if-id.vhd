df664502bc48e07a75c8d1cf7061b2308a861f60
ashleymcpherson <156880729+ashleymcpherson@users.noreply.github.com>
2026-03-22 15:30:49 -0700

    Update if-id.vhd

--- ECE449Main.srcs/sources_1/new/pipeline_registers/if-id.vhd
+++ ECE449Main.srcs/sources_1/new/pipeline_registers/if-id.vhd
@@ -3,28 +3,34 @@ use ieee.std_logic_1164.all;
 
 entity if_id_register is
 port(
-  clock : in  std_logic;
-  reset : in  std_logic;
-  instr_in : in  std_logic_vector(15 downto 0);   -- input 16 bit instruction from instruction memory
-  instr_out : out std_logic_vector(15 downto 0)    -- outout 16 bit instruction to Decode stage
+  clock     : in  std_logic;
+  reset     : in  std_logic;
+  stall     : in  std_logic;
+  flush     : in  std_logic;
+  instr_in  : in  std_logic_vector(15 downto 0);
+  pc_in     : in  std_logic_vector(15 downto 0);
+  instr_out : out std_logic_vector(15 downto 0);
+  pc_out    : out std_logic_vector(15 downto 0)
 );
 end entity;
 
 architecture rtl of if_id_register is
-  signal instr_if_id : std_logic_vector(15 downto 0);   -- for internal storage/internal register
+  signal instr_if_id : std_logic_vector(15 downto 0);
+  signal pc_if_id    : std_logic_vector(15 downto 0);
 begin
-
   process(clock)
   begin
-    if rising_edge(clock) then            -- On the rising edge of the clock...
-      if reset = '1' then                 -- If reset is active
-        instr_if_id <= (others => '0');   -- clear the stored instruction (set all 16 bits to 0)
-      else                                -- If reset is inactive
-        instr_if_id <= instr_in;          -- copy input instruction into the internal register
+    if rising_edge(clock) then
+      if reset = '1' or flush = '1' then
+        instr_if_id <= (others => '0');
+        pc_if_id    <= (others => '0');
+      elsif stall = '0' then
+        instr_if_id <= instr_in;
+        pc_if_id    <= pc_in;
       end if;
     end if;
   end process;
 
-  instr_out <= instr_if_id;   -- output is whatever we stored last clock
-
+  instr_out <= instr_if_id;
+  pc_out    <= pc_if_id;
 end architecture;
