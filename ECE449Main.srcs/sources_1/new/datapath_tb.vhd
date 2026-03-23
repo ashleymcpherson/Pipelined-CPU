30247c188c03e8018a9480cbda5024c4c9717dc8
fluppy27 <106699947+fluppy27@users.noreply.github.com>
2026-03-22 18:07:09 -0700

    updated datapath for format b part 1, need to update clock cylces, when they are known

--- ECE449Main.srcs/sources_1/new/datapath_tb.vhd
+++ ECE449Main.srcs/sources_1/new/datapath_tb.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity datapath_tb is
 end datapath_tb;
@@ -9,12 +10,14 @@ architecture simulate of datapath_tb is
     component datapath is
         Port (
             clk : in std_logic;
-            reset : in std_logic
+            reset : in std_logic;
+            outside_input : in std_logic_vector(15 downto 0)
         );
     end component;
 
     signal clk : std_logic := '0';
     signal reset : std_logic := '0';
+    signal outside_input : std_logic_vector(15 downto 0);
 
     constant clk_period : time := 10 ns;
 
@@ -22,7 +25,8 @@ begin
     uut : datapath
     port map(
         clk => clk,
-        reset => reset
+        reset => reset,
+        outside_input => outside_input
     );
 
     clk_process : process
@@ -36,11 +40,34 @@ begin
     end process;
 
     stimulus_process : process
+        variable cycle : std_logic_vector := "0000";
     begin
+        outside_input <= x"0000";
+        
         reset <= '1';       -- hold reset for active start
         wait for 30 ns;     
         reset <= '0';       -- release reset so PC begins fetching from ROM
         wait for 300 ns;    -- let pipeline run
+        
+        loop
+            wait until rising_edge(clk);
+            cycle := std_logic_vector(unsigned(cycle) + 1);
+            
+            case cycle is
+                when x"FFFF" =>
+                    outside_input <= x"0002";
+                when x"02F4" =>
+                    outside_input <= x"0003";
+                when x"0AEF" =>
+                    outside_input <= x"0001";
+                when x"fffe" =>
+                    outside_input <= x"0005";
+                when others =>
+                    outside_input <= x"0000";
+            end case;
+        end loop;
+        
+        
         wait;
     end process;
 
